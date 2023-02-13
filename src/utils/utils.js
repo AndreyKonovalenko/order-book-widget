25c38de530edb61cd69b5bc516cd061641f90ff4
ak <andreinew@mail.ru>
2023-02-13 16:35:28 +0300

    chore: adde new manageOrderbook function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,3 +12,39 @@ export const findMax = (arr) => {
   const amountArr = arr.map((element) => parseFloat(element[1]));
   return Math.max(...amountArr);
 };
+
+export const manageOrderBook = (
+  snapshotPriceArr,
+  depthUpdatePriceArr,
+  type
+) => {
+  let newArr = snapshotPriceArr.map((element) => element);
+  if (depthUpdatePriceArr.length > 0) {
+    depthUpdatePriceArr.forEach((update) => {
+      const index = snapshotPriceArr.findIndex(
+        (element) => element[0] === update[0]
+      );
+      if (index !== -1) {
+        if (update[1] === 0) {
+          // price level exists, quantity set to 0, remove from list
+          newArr.splice(index, 1);
+        }
+        if (update[1] > 0) {
+          //price level already exists, different quantity
+          newArr[index][1] = update[1];
+        }
+      }
+      if (index === -1) {
+        // new price level, add to list
+        newArr.push(update);
+      }
+    });
+    if (type === "bids") {
+      newArr.sort((a, b) => b[0] - a[0]);
+    }
+    if (type === "asks") {
+      newArr.sort((a, b) => a[0] - b[0]);
+    }
+  }
+  return newArr;
+};
