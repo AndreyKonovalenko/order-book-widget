b465a541e1588c179b7c2818e157a959075b7840
AndreyKonovalenko <andreinew@mail.ru>
2023-02-19 16:10:00 +0300

    chore: work on test

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const formatToCurrency = (num) => {
   return num.toLocaleString(
-    "en-US",
+    'en-US',
     { minimumFractionDigits: 2 },
     {
       maximumFractionDigits: 2,
@@ -18,6 +18,7 @@ export const manageOrderBook = (
   depthUpdatePriceArr,
   type
 ) => {
+  //console.log(snapshotPriceArr, depthUpdatePriceArr, type);
   let newArr = snapshotPriceArr.map((element) => element);
   let sorted = true;
   if (depthUpdatePriceArr.length > 0) {
@@ -25,9 +26,8 @@ export const manageOrderBook = (
       const index = snapshotPriceArr.findIndex(
         (element) => element[0] === update[0]
       );
-      console.log(index);
       if (index !== -1) {
-        if (update[1] === "0") {
+        if (update[1] === '0') {
           // price level exists, quantity set to 0, remove from list
           newArr.splice(index, 1);
         }
@@ -37,21 +37,21 @@ export const manageOrderBook = (
         }
       }
 
-      if (index === -1) {
+      if (index === -1 && update[1] !== '0') {
         // new price level, add to list
         newArr.push(update);
         sorted = false;
       }
     });
     if (sorted === false) {
-      if (type === "bids") {
+      if (type === 'bids') {
         newArr.sort((a, b) => b[0] - a[0]);
       }
-      if (type === "asks") {
+      if (type === 'asks') {
         newArr.sort((a, b) => a[0] - b[0]);
       }
     }
   }
-  console.log(newArr);
+  console.log(snapshotPriceArr);
   return newArr;
 };
