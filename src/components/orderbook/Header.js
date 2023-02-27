265d926c99bb3660e6648db953d173967e9bd338
ak <andreinew@mail.ru>
2023-02-27 11:12:31 +0300

    chore: fix header line break issue on mobile safari

--- src/components/orderbook/Header.js
+++ src/components/orderbook/Header.js
@@ -1,42 +1,43 @@
-import theme from '../../theme/theme';
+import theme from "../../theme/theme";
 const Header = () => {
   const { colors, fonts } = theme;
   const styles = {
     container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      padding: '15px 0px 0px 15px',
-      width: '185px',
-      height: '44px',
-      flex: 'none',
-      order: '0',
-      flexGrow: '0',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      padding: "15px 0px 0px 15px",
+      width: "185px",
+      height: "44px",
+      flex: "none",
+      order: "0",
+      flexGrow: "0",
     },
     textContainer: {
-      padding: '0px',
+      padding: "0px",
+      width: "100%",
     },
     head0: {
       fontFamily: fonts.family.main,
-      fontWeight: '700',
+      fontWeight: "700",
       fontSize: fonts.size.head,
       color: colors.text.main,
-      lineHeight: '116%',
-      margin: '0px',
+      lineHeight: "116%",
+      margin: "0px",
     },
     head1: {
       fontFamily: fonts.family.main,
-      fontWeight: '400',
+      fontWeight: "400",
       fontSize: fonts.size.head,
       color: colors.buy,
-      lineHeight: '116%',
-      margin: '0px',
+      lineHeight: "116%",
+      margin: "0px",
     },
     head2: {
       color: colors.sell,
     },
   };
-  const text = 'and selling';
+  const text = "and selling";
   return (
     <div style={styles.container}>
       <div style={styles.textContainer}>
